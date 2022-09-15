35bc8b3f266000011c734e80e98132b33ee63a75
LogOS0408 <bsv-81@mail.ru>
2022-09-15 16:21:25 +0400

    2. Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,6 +4,18 @@ public class Item {
     private int id;
     private String name;
 
+    public Item(int id, String name) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item() {
+    }
+
     public int getId() {
         return id;
     }
