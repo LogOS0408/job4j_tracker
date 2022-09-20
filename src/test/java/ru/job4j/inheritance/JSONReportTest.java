8d889caf9f1c8e945d7502a7ffcd179e1e67ef82
LogOS0408 <bsv-81@mail.ru>
2022-09-20 10:35:05 +0400

    4. Переопределение [#174890 #344418]

--- src/test/java/ru/job4j/inheritance/JSONReportTest.java
+++ src/test/java/ru/job4j/inheritance/JSONReportTest.java
@@ -20,7 +20,6 @@ class JSONReportTest {
         assertThat(result).isEqualTo(expected);
     }
 
-
     @Test
     public void whenNameIsDavidBodyIsNameSong() {
         String ln = System.lineSeparator();
